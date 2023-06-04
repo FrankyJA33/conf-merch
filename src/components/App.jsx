9ebc0a1ad35ffd55dbb9b382eef38f7f3147bf77
Francisco Leon <pac6831@hotmail.com>
2023-06-03 21:25:12 -0600

    Configuraciones iniciales del proyecto

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600">hola mundo!!!</h1>
+    </div>
+  );
+};
+
+export { App };
