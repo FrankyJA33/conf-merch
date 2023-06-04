5abc5017697cfb5c3a3b34d41c0e4e3ac6b209c8
Francisco Leon <pac6831@hotmail.com>
2023-06-04 01:09:51 -0600

    Estructura del proyecto, agregando react-router para la navegacion, primera version del home, y agregando estilos a toda la pagina

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,62 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(s*)css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-          'sass-loader',
-          'postcss-loader',
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+        {
+            test: /\.(s*)css$/,
+            use:[
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                },
+                'css-loader',
+                'sass-loader',
+                'postcss-loader',
+            ]
+        }
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+      }),
 
-    new MiniCssExtractPlugin({
-      filename: 'assests/[name].css',
-    }),
-  ],
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3005,
-  },
-};
+      new MiniCssExtractPlugin({
+          filename: 'assests/[name].css',
+      })
+    ],
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      compress: true,
+      historyApiFallback: true,
+      port: 3005,
+    },
+  };
